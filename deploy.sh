8cc2610d89f1bad92b20cb5fe24e0a35893ed553
Valeria Berdnikova <likespony@gmail.com>
2020-02-02 11:17:33 +0200

    deploy update

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-ssh $DEPLOYMENT_SERVER_USERNAME@$DEPLOYMENT_SERVER_IP << 'ENDSSH'
- mkdir -p ~/dj
- scp -r ./* $DEPLOYMENT_SERVER_USERNAME@$DEPLOYMENT_SERVER_IP:~/dj
- cd ~/dj
- docker login -u gitlab-ci-token -p $CI_JOB_TOKEN $CI_REGISTRY
- docker pull $CI_REGISTRY/$CI_PROJECT_PATH
- docker-compose -f $DOCKER_COMPOSE_FILE_PATH up -d
-ENDSSH
